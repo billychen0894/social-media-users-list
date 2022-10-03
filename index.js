c155b57d8adc56daf1c66cdd0f1465fc0c7e3666
Billy Chen <billychen0894@gmail.com>
2022-10-02 19:31:53 -0700

    Add search & add friends features

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const BASE_URL = 'https://lighthouse-user-api.herokuapp.com/'
 const INDEX_URL = BASE_URL + 'api/v1/users/'
 const userListPanel = document.getElementById('user-list')
+const searchForm = document.getElementById('searchForm')
+const searchInput = document.getElementById('searchInput')
+const addToFriendsBtn = document.getElementById('btn-addToFriends')
 
 const userList = []
+let filteredUserList = []
 
 function renderUserList(data) {
   let rawHTML = ''
@@ -16,12 +20,13 @@ function renderUserList(data) {
             data-bs-toggle="modal"
             data-bs-target="#staticBackdrop"
             data-id="${user.id}"/>
-          <h6 id="user-name" class="mt-2">${user.name + user.surname}</h6>
+          <h6 id="user-name" class="mt-2">${user.name + ' ' + user.surname}</h6>
         </div>`
   })
   userListPanel.innerHTML = rawHTML
 }
 
+// Show user info
 function showUserInfo(id) {
   const userImage = document.getElementById('user-image')
   const userName = document.getElementById('modal-title')
@@ -42,10 +47,24 @@ function showUserInfo(id) {
       userBirth.innerText = `Birthday: ${data.birthday}`
       userEmail.innerText = `Email: ${data.email}`
       userImage.innerHTML = `<img src="${data.avatar}" alt="user-avatar" />`
+      addToFriendsBtn.setAttribute('data-id', `${data.id}`)
     })
     .catch((error) => {
       console.log(error.message)
     })
+}
+
+// Add friends to localStorage
+function addToFriends(id) {
+  const friendsList = JSON.parse(localStorage.getItem('friendsList')) || []
+  const user = userList.find((user) => user.id === id)
+
+  if (friendsList.some((friend) => friend.id === id)) {
+    alert(`${user.name + ' ' + user.surname} is already in your friend list.`)
+  } else {
+    friendsList.push(user)
+    localStorage.setItem('friendsList', JSON.stringify(friendsList))
+  }
 }
 
 // GET request to get all users data and render all users
@@ -65,4 +84,25 @@ userListPanel.addEventListener('click', (event) => {
   if (event.target.matches('#user-avatar')) {
     showUserInfo(event.target.dataset.id)
   }
+})
+
+// To search users with search keywords
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const input = searchInput.value.trim().toLowerCase()
+
+  // if input has spaces only, remove spaces and will not do anything
+  if (!input.length) return (searchInput.value = '')
+
+  filteredUserList = userList.filter(function(user) {
+    const fullName = user.name + ' ' + user.surname
+    return fullName.toLowerCase().includes(input)
+  })
+  renderUserList(filteredUserList)
+})
+
+// Add to Friends list
+addToFriendsBtn.addEventListener('click', (event) => {
+  const userId = event.target.dataset.id
+  addToFriends(Number(userId))
 })
